2f6c98f264450cb1cc8d21a02f0b4acf35b7f38b
Jakub Jaksik <jjaksik@match-trade.com>
2026-05-22 16:57:22 +0200

    fix: build

--- crates/tick-trigger-fan-out-bench/build.rs
+++ crates/tick-trigger-fan-out-bench/build.rs
@@ -3,6 +3,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .build_server(false)
         .build_client(true)
+        .protoc_arg("--experimental_allow_proto3_optional")
         .compile_protos(
             &[
                 format!("{}/searcher.proto", proto_dir),
